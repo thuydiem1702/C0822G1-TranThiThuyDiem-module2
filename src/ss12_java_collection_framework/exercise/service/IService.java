1864eab7e3345e4310e84554caef3e97f60b03d8
thuydiem1702 <thuydiem17022001@gmail.com>
2022-10-20 20:04:49 +0700

    up

---
+++ src/ss12_java_collection_framework/exercise/service/IService.java
@@ -0,0 +1,10 @@
+package ss12_java_collection_framework.exercise.service;
+
+public interface IService {
+    void dispaly();
+    void add();
+    void edit();
+    void delete();
+    void searchByName(String name);
+    void sort();
+}
---
+++ src/ss12_java_collection_framework/exercise/view/ProductView.java
@@ -0,0 +1,9 @@
+package ss12_java_collection_framework.exercise.view;
+
+import ss12_java_collection_framework.exercise.controller.MainProduct;
+
+public class ProductView {
+    public static void main(String[] args) {
+        MainProduct.displayMainMenu();
+    }
+}
