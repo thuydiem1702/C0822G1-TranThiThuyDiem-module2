1943bd5f630cfcd606271499626686686046be3e
thuydiem1702 <thuydiem17022001@gmail.com>
2022-10-25 18:42:24 +0700

    up

---
+++ src/ss15_exception_debug/exercise/IllegalTriangleExceptionTest.java
@@ -0,0 +1,10 @@
+package ss15_exception_debug.exercise;
+
+public class IllegalTriangleExceptionTest extends Exception {
+    public IllegalTriangleExceptionTest() {
+    }
+
+    public IllegalTriangleExceptionTest(String message) {
+        super(message);
+    }
+}
---
+++ src/ss15_exception_debug/exercise/Triangle.java
@@ -0,0 +1,56 @@
+package ss15_exception_debug.exercise;
+
+public class Triangle extends IllegalTriangleException {
+    private double a, b, c;
+
+    public Triangle() {
+    }
+
+    public Triangle(double a, double b, double c) throws IllegalTriangleExceptionTest {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalTriangleExceptionTest(" canh ko duoc be hon hoac bang ko ");
+        } else if (a + b < c || a + c < b || c + b < a) {
+            throw new IllegalTriangleExceptionTest(" tong 2 canh lon hon canh con lai");
+        } else {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public void check(int a, int b, int c) throws IllegalTriangleExceptionTest {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalTriangleExceptionTest(" canh ko duoc be hon hoac bang ko ");
+        } else if (a + b <= c || a + c <= b || c + b <= a) {
+            throw new IllegalTriangleExceptionTest(" tong 2 canh lon hon canh con lai");
+        } else {
+            setA(a);
+            setB(b);
+            setC(c);
+        }
+    }
+}
