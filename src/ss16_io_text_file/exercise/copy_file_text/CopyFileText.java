343af4311df4753310901bf57102d69dc873ceea
thuydiem1702 <thuydiem17022001@gmail.com>
2022-10-26 16:45:22 +0700

    up

--- src/ss16_io_text_file/exercise/copy_file_text/CopyFileText.java
+++ src/ss16_io_text_file/exercise/copy_file_text/CopyFileText.java
@@ -11,9 +11,14 @@ public class CopyFileText {
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         List<String> list = new ArrayList<>();
         String line;
+        String[] array;
+        int sum=0;
         while ((line = bufferedReader.readLine()) != null) {
+            array = line.split("");
+            sum += array.length;
             list.add(line);
         }
+        System.out.println("Số kí tự trong tệp: " + sum);
         bufferedReader.close();
 
         File file1 = new File("src\\ss16_io_text_file\\exercise\\copy_file_text\\target.txt");
